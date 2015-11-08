74dba0eacb2753686d1e1543bdb9c58644be971e
Dominique Canlas <dcanlas@gsngames.com>
2015-11-08 14:42:38 -0800

    setting up project in consulo

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+	
+	void Start () {
+		if (instance != null && instance != this) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+		
+	}
+}
